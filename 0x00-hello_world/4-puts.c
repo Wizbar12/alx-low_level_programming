c7f092c28e6582ba2f5ba3786cde389c6f865b65
modenwa <wizboy95.ww@gmail.com>
2023-08-10 11:26:02 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /**
  * main - Program that print line with puts function
  * Return: 0
- */c programming running
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
