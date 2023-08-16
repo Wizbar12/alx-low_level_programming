eee7a295c01c86a67b288c6c019a8dd52e21c549
modenwa <wizboy95.ww@gmail.com>
2023-08-16 08:37:30 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - add two value
+ * @n1:  the first number to added
+ * @n2: the second number to be added
+ * return: integer value
+ */
+
+int add(int n1, int n2)
+{
+return (n1 + n2);
+}
