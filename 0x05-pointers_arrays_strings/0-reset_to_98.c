23b1ebc974a0d41436c46a686e376f3e1f0f02fa
modenwa <wizboy95.ww@gmail.com>
2023-08-21 03:05:35 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * rest_to_98 - function that takes a pointerto an int as paramater
+ * and updates the value it points to 98
+ * @n: input
+ * return: n
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
