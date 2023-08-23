a086c947daa9dbbfe1b259f79201c172b56863f6
modenwa <wizboy95.ww@gmail.com>
2023-08-23 10:51:19 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcat concatenates two strings
+ * @dest: input value
+ * @src: input value
+ * return: void
+ */
+char _strcat(char *dest, char *src)
+{
+int m;
+int w;
+
+m = 0;
+while (dest[m] != '\0')
+{
+	m++;
+}
+while (src[w] != '\0')
+{
+	dest[m] = src[w];
+	m++;
+	w++;
+}
+dest[m] = '\0';
+return (dest);
+}
