219f41fa2c29761c49b739409a0eee4f750c1df8
modenwa <wizboy95.ww@gmail.com>
2023-09-06 12:02:20 -0700

    2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib>
+/**str_concat - concertrate two strings
+ * @s1: first sring
+ * @s2: second string
+ *
+ * Return: pointer to the conc... string
+ * which memory was allocated for.
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	size_t len1, len2, i, k;
+	char *result;
+
+	len1 = len2 = 0;
+
+	if (s1 != NULL)
+	{
+		i = 0;
+		while (s1[i++] != '\0')
+			len1++;
+
+	}
+	if (s2 != NULL)
+	{
+		i = 0;
+		while (s2[i++] != '\0')
+			len2++;
+	}
+	len = len1 + len2;
+	result = (char *)malloc(sizeof(char) * (len + 1));
+	if (result == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+		result[i] = s1[i];
+	for (j = 0; j < len2; j++)
+		result[j] = s2[j];
+	result[len] = '\0';
+
+	return (result);
+}
